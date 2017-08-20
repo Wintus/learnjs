052c608861f4cd4f7c695f74f31dcd27b48fbe36
Yuya <yuya.maemichi@gmail.com>
2017-08-21 05:12:00 +0900

    Add answer checking

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -37,6 +37,22 @@ describe('LearnJS', function() {
       expect(view.find('[data-name="code"]').text())
         .toEqual("function problem() { return __; }");
     });
+
+    describe('answer section', function() {
+      it('can check a correct answer by hitting a button', function() {
+        view.find('.answer').val('true');
+        view.find('.check-btn').click();
+        expect(view.find('.result').text())
+          .toEqual('Correct!');
+      });
+
+      it('can reject an incorrect answer', function() {
+        view.find('.answer').val('false');
+        view.find('.check-btn').click();
+        expect(view.find('.result').text())
+          .toEqual('Incorrect!');
+      });
+    });
   });
 
   it('invokes the router when loaded', function () {
