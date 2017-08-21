902228bd3444f796769f7dd56ac20f862ead9be8
Yuya <yuya.maemichi@gmail.com>
2017-08-22 02:18:17 +0900

    reformat spec

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -17,6 +17,19 @@ describe('LearnJS', function() {
     expect(learnjs.problemView).toHaveBeenCalledWith('42');
   });
 
+  it('invokes the router when loaded', function () {
+    spyOn(learnjs, 'showView');
+    learnjs.appOnReady();
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+
+  it('subscribes to the hash change event', function () {
+    learnjs.appOnReady();
+    spyOn(learnjs, 'showView');
+    $(window).trigger('hashchange');
+    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+  });
+
   describe('problem view', function() {
     var view;
     beforeEach(function() {
@@ -61,17 +74,4 @@ describe('LearnJS', function() {
     });
 */
   });
-
-  it('invokes the router when loaded', function () {
-    spyOn(learnjs, 'showView');
-    learnjs.appOnReady();
-    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
-  });
-
-  it('subscribes to the hash change event', function () {
-    learnjs.appOnReady();
-    spyOn(learnjs, 'showView');
-    $(window).trigger('hashchange');
-    expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
-  });
-})
+});
