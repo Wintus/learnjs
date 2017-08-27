ec67130b75db93db1dd9353404722f8ab8b46072
Yuya <yuya.maemichi@gmail.com>
2017-08-28 03:33:25 +0900

    Refactor spec by Jasmine jQuery

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -3,12 +3,12 @@
 describe('LearnJS', function() {
   it('can show a problem view', function () {
     learnjs.showView('#problem-1');
-    expect($('.view-container .problem-view').length).toEqual(1);
+    expect($('.view-container .problem-view')).toHaveLength(1);
   });
 
   it('shows the landing page view when there is no hash', function () {
     learnjs.showView('');
-    expect($('.view-container .landing-view').length).toEqual(1);
+    expect($('.view-container .landing-view')).toHaveLength(1);
   });
 
   it('passes the hash view parameter to the view function', function () {
@@ -37,7 +37,7 @@ describe('LearnJS', function() {
     spyOn(elem, 'fadeIn');
     learnjs.flashElement(elem, text);
 
-    expect(elem.text()).toEqual(text);
+    expect(elem).toHaveText(text);
     expect(elem.fadeOut).toHaveBeenCalled();
     expect(elem.fadeIn).toHaveBeenCalled();
   });
@@ -45,8 +45,8 @@ describe('LearnJS', function() {
   it('can redirect to the main view after the last problem is answered', function() {
     var flash = learnjs.buildCorrectFlash(2);
     var link = flash.find('a');
-    expect(link.attr('href')).toEqual('');
-    expect(link.text()).toEqual("You're Finished!");
+    expect(link).toHaveAttr('href', '');
+    expect(link).toHaveText("You're Finished!");
   });
 
   it('can trigger events on the view', function() {
@@ -67,38 +67,35 @@ describe('LearnJS', function() {
     });
 
     it('has a title that includes the problem number', function() {
-      expect(view.find('.title').text())
-        .toEqual('Problem #1');
+      expect(view.find('.title')).toHaveText('Problem #1');
     });
 
     it('shows the description', function() {
-      expect(view.find('[data-name="description"]').text())
-        .toEqual('What is truth?');
+      expect(view.find('[data-name="description"]')).toHaveText('What is truth?');
     });
 
     it('shows the problem code', function() {
-      expect(view.find('[data-name="code"]').text())
-        .toEqual("function problem() { return __; }");
+      expect(view.find('[data-name="code"]')).toHaveText("function problem() { return __; }");
     });
 
     describe('skip button', function() {
       it('is added to the navbar when the view is added', function() {
-        expect($('.nav-list .skip-btn').length).toEqual(1);
+        expect($('.nav-list .skip-btn')).toHaveLength(1);
       });
 
       it('is removed from the navbar when the view is removed', function() {
         view.trigger('removingView');
-        expect($('.nav-list .skip-btn').length).toEqual(0);
+        expect($('.nav-list .skip-btn')).toHaveLength(0);
       });
 
       it('contains a link to the next problem', function() {
-        expect($('.nav-list .skip-btn').length).toEqual(1);
+        expect($('.nav-list .skip-btn')).toHaveLength(1);
       });
 
       it('does not added when at the last problem', function() {
         view.trigger('removingView');
         view = learnjs.problemView(2);
-        expect($('.nav-list .skip-btn').length).toEqual(0);
+        expect($('.nav-list .skip-btn')).toHaveLength(0);
       });
     });
 
@@ -106,15 +103,13 @@ describe('LearnJS', function() {
       it('can check a correct answer by hitting a button', function() {
         view.find('.answer').val('true');
         view.find('.check-btn').click();
-        expect(view.find('.result').text())
-          .toMatch(/Correct!\s+Next Problem/);
+        expect(view.find('.result')).toHaveText(/Correct!\s+Next Problem/);
       });
 
       it('can reject an incorrect answer', function() {
         view.find('.answer').val('false');
         view.find('.check-btn').click();
-        expect(view.find('.result').text())
-          .toEqual('Incorrect!');
+        expect(view.find('.result')).toHaveText('Incorrect!');
       });
     });
   });
